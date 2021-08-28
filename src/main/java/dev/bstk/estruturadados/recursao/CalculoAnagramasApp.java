5d995591ad6240dcfd99621d15c378e4fa56e556
Bruno Luz <bsilva.se@gmail.com>
2021-08-27 22:45:53 -0300

    main - Implementando anagramas

--- src/main/java/dev/bstk/estruturadados/recursao/CalculoAnagramasApp.java
+++ src/main/java/dev/bstk/estruturadados/recursao/CalculoAnagramasApp.java
@@ -2,4 +2,7 @@ package dev.bstk.estruturadados.recursao;
 
 public class CalculoAnagramasApp {
 
+    public static void main(String[] args) {
+        CalculoAnagramas.calcularAnagrama(10);
+    }
 }
